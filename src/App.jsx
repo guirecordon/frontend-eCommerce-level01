f70f33ec71005d59874f9f1395e9092817854476
Gui Recordon <guilherme.recordon@gmail.com>
2022-12-29 22:28:19 -0300

    Set up router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+
 export function App() {
-  return <h1>eCommerce Store</h1>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,3 @@
+export function Product() {
+  return <h1>A single product</h1>;
+}
